b117131b39387089368ff069fa78fd2775fcfe5a
hdh4202 <aaabbb4202@naver.com>
2023-08-11 17:57:19 +0900

    COMMIT sqlToElastic Friday last version

--- sqlToElastic.py
+++ sqlToElastic.py
@@ -23,84 +23,65 @@ es_port = info.es_port
 es_username = info.es_username
 es_password = info.es_password
 
-def convert_to_iso8601_format(dt):
-    # 'datetime' 객체를 'ISO 8601' 형식으로 변환하여 반환
-    # print(dt.strftime('%Y-%m-%dT%H:%M:%S'))
-    # return dt.strftime('%Y-%m-%dT%H:%M:%S')
-    # print(dt.strftime('%Y-%m-%d %H:%M:%S'))
+es = Elasticsearch(
+    [f"{es_host}:{es_port}"],
+    http_auth=(es_username, es_password)    
+)
+
+
+def convert_to_iso8601_format(dt):    
     return dt.strftime('%Y-%m-%d %H:%M:%S')
 
 
-# MySQL 데이터 가져오기
-def get_data_from_mysql():
-    connection = mysql.connector.connect(
-        host=mysql_host,
-        port = mysql_port,
-        user=mysql_user,        
-        password=mysql_password,
-        database=mysql_database
-    )
+connection = mysql.connector.connect(
+    host=mysql_host,
+    port = mysql_port,
+    user=mysql_user,        
+    password=mysql_password,
+    database=mysql_database
+)
 
-    cursor = connection.cursor()
+cursor = connection.cursor()
 
-    prompt_combine = '''
-    SELECT
-        p1.image_id,
-        p1.prompt AS prompt,
-        p2.prompt AS negative_prompt,
-        p1.prompt_time As timestamp
-    FROM image_prompt p1
-    JOIN image_prompt p2 ON p1.image_id = p2.image_id
-    WHERE p1.is_positive = true AND p2.is_positive = false
-    '''
+prompt_combine = '''
+SELECT
+    p1.image_id,
+    p1.prompt AS prompt,
+    p2.prompt AS negative_prompt,
+    p1.prompt_time As timestamp
+FROM image_prompt p1
+JOIN image_prompt p2 ON p1.image_id = p2.image_id
+WHERE p1.is_positive = true AND p2.is_positive = false
+'''
 
-    cursor.execute(prompt_combine)    
+cursor.execute(prompt_combine)    
 
 
-    
-    # query = f"SELECT * FROM IMAGE_PROMPT;"
-    
-    # cursor.execute(query)
 
-    columns = [column[0] for column in cursor.description]
-    data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+# query = f"SELECT * FROM IMAGE_PROMPT;"
 
-    cursor.close()
-    connection.close()   
+# cursor.execute(query)
 
-    return data
+columns = [column[0] for column in cursor.description]
+data = [dict(zip(columns, row)) for row in cursor.fetchall()]
 
-# Elasticsearch에 bulk로 데이터 색인
-def index_data_to_elasticsearch(data):
-    # TLS 설정을 위한 SSLContext 생성
-    # ssl_context = ssl.create_default_context(cafile=ca_certs)
+cursor.close()
+connection.close()
 
-    # Elasticsearch 클라이언트 생성
-    es = Elasticsearch(
-        [f"{es_host}:{es_port}"],
-        http_auth=(es_username, es_password),
-        scheme="http"
-    )
-
-    #Elasticsearch에 저장할 데이터 변환 (index와 id 필드가 있는 dict 형태로 변환)
-    actions = [
-        {
-            "_index": "test_image_prompt",
-            "_id": doc["image_id"],            
-            "_source": {
-                "image_id" : doc["image_id"],
-                "prompt" : doc["prompt"],
-                "negative_prompt" : doc["negative_prompt"],                
-                "timestamp":f'{convert_to_iso8601_format(doc["timestamp"])}'                
-            }
+#Elasticsearch에 저장할 데이터 변환 (index와 id 필드가 있는 dict 형태로 변환)
+actions = [
+    {
+        "_index": "test_image_prompt",
+        "_id": doc["image_id"],            
+        "_source": {
+            "image_id" : doc["image_id"],
+            "prompt" : doc["prompt"],
+            "negative_prompt" : doc["negative_prompt"],                
+            "timestamp":f'{convert_to_iso8601_format(doc["timestamp"])}'                
         }
-        for doc in data
-    ]
-  
-    # Elasticsearch에 bulk 색인 실행
-    bulk(es, actions)
+    }
+    for doc in data
+]
 
-if __name__ == "__main__":
-    data_from_mysql = get_data_from_mysql()
-    index_data_to_elasticsearch(data_from_mysql)
-
+# Elasticsearch에 bulk 색인 실행
+bulk(es, actions)
